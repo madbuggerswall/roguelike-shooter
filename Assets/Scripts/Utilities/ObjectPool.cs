c46143a767d870f7fddf402159a8c558202ead54
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2023-02-26 15:52:07 +0300

    Generic object pool

--- Assets/Scripts/Utilities/ObjectPool.cs
+++ Assets/Scripts/Utilities/ObjectPool.cs
@@ -56,4 +56,40 @@ public class ObjectPool : MonoBehaviour {
 		spawnedObject.SetActive(true);
 		return spawnedObject;
 	}
+
+	// Generic
+	T addObject<T>(T prefab) where T : MonoBehaviour {
+		prefab.gameObject.SetActive(false);
+
+		T pooledObject = GameObject.Instantiate(prefab);
+		pooledObject.name = prefab.name;
+		pooledObject.transform.SetParent(transform);
+		pool.Add(pooledObject.gameObject);
+		return pooledObject;
+	}
+
+	T getObject<T>(T prefab) where T : MonoBehaviour {
+		foreach (GameObject pooledObject in pool) {
+			if (prefab.gameObject.name == pooledObject.name && !pooledObject.activeInHierarchy) {
+				return pooledObject.GetComponent<T>();
+			}
+		}
+		return addObject(prefab);
+	}
+
+	public T spawn<T>(T prefab) where T : MonoBehaviour {
+		T spawnedObject = getObject(prefab);
+		spawnedObject.transform.position = Vector3.zero;
+		spawnedObject.transform.eulerAngles = Vector3.zero;
+		spawnedObject.gameObject.SetActive(true);
+		return spawnedObject;
+	}
+	
+	public T spawn<T>(T prefab, Vector3 position) where T : MonoBehaviour {
+		T spawnedObject = getObject(prefab);
+		spawnedObject.transform.position = position;
+		spawnedObject.transform.eulerAngles = Vector3.zero;
+		spawnedObject.gameObject.SetActive(true);
+		return spawnedObject;
+	}
 }
