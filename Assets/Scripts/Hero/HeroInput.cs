9d66f416449c073ee163b594e2059fab8e5ad567
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2023-02-27 22:18:03 +0300

    Input functions for PlayerInput. Unfortunately it adds 10 ms to main thread

--- Assets/Scripts/Hero/HeroInput.cs
+++ Assets/Scripts/Hero/HeroInput.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 [DefaultExecutionOrder(-4)]
 public class HeroInput : MonoBehaviour {
 
+	UnityAction controller;
 	Vector2 input;
 
 	void Update() {
 		keyboardController();
 	}
 
-	// void OnMove(InputValue value) { moveDirection = value.Get<Vector2>(); }
+	public void onMove(InputAction.CallbackContext context) {
+		input = context.ReadValue<Vector2>();
+		input.Normalize();
+	}
+
+	public void onTouchMove(InputAction.CallbackContext context) {
+		float deltaMul = 2f;
+		input += deltaMul * Time.deltaTime * context.ReadValue<Vector2>();
+		input.Normalize();
+	}
+
+	public void onTouch(InputAction.CallbackContext context) {
+		if (context.canceled)
+			input = Vector2.zero;
+	}
 
 	// Prototyping
 	void keyboardController() {
@@ -33,5 +49,31 @@ public class HeroInput : MonoBehaviour {
 		}
 	}
 
+	void mobileController() {
+		float deltaMul = 2f;
+		input.y += deltaMul * Time.deltaTime * Touchscreen.current.primaryTouch.delta.y.ReadValue();
+		input.x += deltaMul * Time.deltaTime * Touchscreen.current.primaryTouch.delta.x.ReadValue();
+
+		// if (Touchscreen.current.primaryTouch.phase == ) {
+		// 	input.x = input.y = 0;
+		// 	Debug.Log("No touches");
+		// }
+
+		input.Normalize();
+		// input = Vector2.ClampMagnitude(input, 1f);
+	}
+
+	void initializeController() {
+		bool isAndroid = Application.platform == RuntimePlatform.Android;
+		bool isIOS = Application.platform == RuntimePlatform.IPhonePlayer;
+
+		if (isAndroid || isIOS) {
+			controller = mobileController;
+		} else {
+			controller = keyboardController;
+		}
+	}
+
+	// Getters
 	public Vector2 getInput() { return input; }
 }
